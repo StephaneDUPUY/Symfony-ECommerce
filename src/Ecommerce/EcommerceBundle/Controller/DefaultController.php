2a864bd241a574029ed200d3ce8a7ba34bc6286b
Stéphane DUPUY <sdupuy.contact@gmail.com>
2021-01-24 15:39:16 +0100

    first bundle with route-controller-vue and variables passed

--- src/Ecommerce/EcommerceBundle/Controller/DefaultController.php
+++ src/Ecommerce/EcommerceBundle/Controller/DefaultController.php
@@ -6,8 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($firstName, $lastName) # have to match with url variables of routing
     {
-        return $this->render('EcommerceBundle:Default:index.html.twig');
+        $wife = 'Emilie';
+        return $this->render('EcommerceBundle:Default:index.html.twig', array('firstname' => $firstName, 'lastname' => $lastName, 'wife' => $wife));
+        # $this->render = views folder
+        # EcommerceBundle:Default = EcommerceBundle/Default folder
+        # return index view from "views/Default" folder with array and its "value => case" content
     }
 }
